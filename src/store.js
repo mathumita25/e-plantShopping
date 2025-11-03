bc9d9db8db922de46bc3c3f59decfcfc4808189f
mathumita25 <mathumita.s2022@vitstudent.ac.in>
2025-11-03 10:24:39 -0500

    Configured Redux store and linked cartReducer in store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
+// Import configureStore from Redux Toolkit
 import { configureStore } from '@reduxjs/toolkit';
+
+// Import the cart reducer (manages cart slice)
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store using configureStore from Redux Toolkit
+const store = configureStore({
+  // Define the root reducer object
+  reducer: {
+    // 'cart' is the name of the slice in the store, and it's managed by cartReducer
+    cart: cartReducer,
+  },
 });
-export default store
+
+// Export the store for use throughout the app (e.g., in <Provider store={store}>)
+export default store;
